0347894fd82893507a76c0919503a02f9224765e
Ammar Askar <ammar@ammaraskar.com>
2015-04-02 21:14:44 +0500

    Read the docs will always default to their theme, no need to explicitly define it there

--- docs/conf.py
+++ docs/conf.py
@@ -127,8 +127,6 @@ if os.environ.get("READTHEDOCS", "") != "True":
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
   except ImportError:
     html_theme = "classic"
-else:
-  html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
