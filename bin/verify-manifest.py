fff3947b16b7213f2a1c0d189cab342ce6648779
Jeppe Klitgaard <jeppe@dapj.dk>
2015-04-05 23:13:15 +0200

    PEP8-friendliness, spaces instead of tabs

--- bin/verify-manifest.py
+++ bin/verify-manifest.py
@@ -1,17 +1,19 @@
-import os, subprocess
-import errno, sys
+import os
+import subprocess
+import errno
+import sys
 
 EXPECTED_OUTPUT = "lists of files in version control and sdist match"
 
 
 def check_manifest():
-	os.chdir('..')
-	output = subprocess.check_output(["check-manifest"]).decode()
-	print(output)
-	if EXPECTED_OUTPUT == output.strip():
-		return 1
-	else:
-		sys.exit(2)
+    os.chdir('..')
+    output = subprocess.check_output(["check-manifest"]).decode()
+    print(output)
+    if EXPECTED_OUTPUT == output.strip():
+        return 1
+    else:
+        sys.exit(2)
 
 if __name__ == '__main__':
-	check_manifest()
+    check_manifest()
