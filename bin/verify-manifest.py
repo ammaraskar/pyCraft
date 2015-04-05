902d11b8e1fd6cc28ca085a1b50e160d90fba1ad
Jeppe Klitgaard <jeppe@dapj.dk>
2015-04-05 23:25:24 +0200

    Spruced up verify-manifest.py

--- bin/verify-manifest.py
+++ bin/verify-manifest.py
@@ -1,19 +1,34 @@
-import os
 import subprocess
-import errno
 import sys
 
 EXPECTED_OUTPUT = "lists of files in version control and sdist match"
 
 
 def check_manifest():
-    os.chdir('..')
+    """
+    Returns a tuple containing ``(output, status)``.
+
+    Status is:
+        ``True`` if ``output`` was as expected (no errors),
+        ``False`` otherwise.
+    """
     output = subprocess.check_output(["check-manifest"]).decode()
-    print(output)
     if EXPECTED_OUTPUT == output.strip():
-        return 1
+        status = True
     else:
-        sys.exit(2)
+        status = False
+
+    return (output, status)
 
 if __name__ == '__main__':
-    check_manifest()
+    output, verified = check_manifest()
+
+    if not verified:
+        print("Check-manifest didn't match.")
+        print("OUTPUT: {}".format(output))
+        print("EXPECTED_OUTPUT: {}".format(EXPECTED_OUTPUT))
+        sys.exit(1)
+
+    else:
+        print("Manifest verified.")
+        sys.exit(0)
