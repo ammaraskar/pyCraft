e31bfb58f050762931bf25bb4ceb896e22408693
Jeppe Klitgaard <jeppe@dapj.dk>
2015-04-06 01:25:16 +0200

    Now verify-manifest.py is windows-compatible again

--- bin/verify-manifest.py
+++ bin/verify-manifest.py
@@ -1,28 +1,28 @@
 """
 Exits with exit code 0 if manifest was verified.
 """
-
-from sh import check_manifest
-from sh import ErrorReturnCode_1
 import sys
+import subprocess
+import errno
+import os
 
 EXPECTED_OUTPUT = "lists of files in version control and sdist match"
-
+os.chdir("..")  # Leave bin/
 
 def verify_manifest():
     """
     Returns a ``str`` containing output from ``check-manifest``.
     """
     try:
-        output = check_manifest().decode()
-    except ErrorReturnCode_1 as e:
-        output = e.stderr.decode() + e.stdout.decode()
+        output = subprocess.check_output("check-manifest",
+                                         stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        output = e.output
 
-    return output
+    return output.decode().strip()
 
 if __name__ == '__main__':
     output = verify_manifest()
-
     print(output)
 
     if output == EXPECTED_OUTPUT:
