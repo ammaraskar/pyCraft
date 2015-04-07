ca81f7685a83993fee9d2b40c81b36d035e8f2cd
Jeppe Klitgaard <jeppe@dapj.dk>
2015-04-07 20:07:47 +0200

    Moved tests around for more logical order. Made tests for Profile object.

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,13 +1,16 @@
-from minecraft.authentication import _raise_from_request
-from minecraft.authentication import _make_request
+from minecraft.authentication import Profile
 from minecraft.authentication import AuthenticationToken
+from minecraft.authentication import _make_request
+from minecraft.authentication import _raise_from_request
 from minecraft.exceptions import YggdrasilError
+
 import requests
 import json
 import unittest
 from .compat import mock
 
 FAKE_DATA = {
+    "id_": "85e2c12b9eab4a7dabf61babc11354c2",
     "access_token": "9c771b61cef241808e129e33d51ea745",
     "client_token": "74076db55d8b4087a607fdeace60a94a",
     "username": "TheBadassMCrafter"
@@ -52,18 +55,97 @@ def should_skip_cred_test():
     return False
 
 
-class Yggdrasil(unittest.TestCase):
+class InitProfile(unittest.TestCase):
     def test_init_no_values(self):
-        a = AuthenticationToken()  # noqa
+        p = Profile()
+        self.assertIs(p.id_, None)
+        self.assertIs(p.name, None)
 
-    def test_init_access_token(self):
-        a = AuthenticationToken(access_token=FAKE_DATA["access_token"])  # noqa
+    def test_init_id(self):
+        p = Profile(id_=FAKE_DATA["id_"])
+        self.assertEqual(p.id_, FAKE_DATA["id_"])
 
-    def test_init_client_token(self):
-        a = AuthenticationToken(client_token=FAKE_DATA["client_token"])  # noqa
+    def test_init_name(self):
+        p = Profile(name=FAKE_DATA["username"])
+        self.assertEqual(p.name, FAKE_DATA["username"])
+
+    def test_init_positional(self):
+        p = Profile(FAKE_DATA["id_"], FAKE_DATA["username"])
+        self.assertEqual(p.id_, FAKE_DATA["id_"])
+        self.assertEqual(p.name, FAKE_DATA["username"])
+
+
+class ToDictProfile(unittest.TestCase):
+    def test_to_dict_no_data(self):
+        p = Profile()
+        with self.assertRaises(AttributeError):
+            p.to_dict()
+
+    def test_to_dict_only_id(self):
+        p = Profile(id_=FAKE_DATA["id_"])
+        with self.assertRaises(AttributeError):
+            p.to_dict()
+
+    def test_to_dict_only_name(self):
+        p = Profile(name=FAKE_DATA["username"])
+        with self.assertRaises(AttributeError):
+            p.to_dict()
+
+    def test_to_dict(self):
+        p = Profile(FAKE_DATA["id_"], FAKE_DATA["username"])
+        d = p.to_dict()
+
+        self.assertEqual(FAKE_DATA["id_"], d["id"])
+        self.assertEqual(FAKE_DATA["username"], d["name"])
+
+
+class BoolProfile(unittest.TestCase):
+    # Checks for boolean state is done via __bool__ on Python 3
+    # We need also to check for the Python 2 version, __nonzero__
+    # and explicitly try __bool__ in case test is run on python 2
+    def test_bool_no_data(self):
+        p = Profile()
+        self.assertFalse(p)
+        self.assertFalse(p.__bool__())
+        self.assertFalse(p.__nonzero__())
+
+    def test_bool_only_id(self):
+        p = Profile(id_=FAKE_DATA["id_"])
+        self.assertFalse(p)
+        self.assertFalse(p.__bool__())
+        self.assertFalse(p.__nonzero__())
+
+    def test_bool_only_name(self):
+        p = Profile(name=FAKE_DATA["username"])
+        self.assertFalse(p)
+        self.assertFalse(p.__bool__())
+        self.assertFalse(p.__nonzero__())
+
+    def test_bool_with_data(self):
+        p = Profile(FAKE_DATA["id_"], FAKE_DATA["username"])
+        self.assertTrue(p)
+        self.assertTrue(p.__bool__())
+        self.assertTrue(p.__nonzero__())
+
+
+class InitAuthenticationToken(unittest.TestCase):
+    def test_init_no_values(self):
+        a = AuthenticationToken()
+        self.assertIs(a.username, None)
+        self.assertIs(a.access_token, None)
+        self.assertIs(a.client_token, None)
 
     def test_init_username(self):
-        a = AuthenticationToken(client_token=FAKE_DATA["username"])  # noqa
+        a = AuthenticationToken(username=FAKE_DATA["username"])
+        self.assertEqual(a.username, FAKE_DATA["username"])
+
+    def test_init_access_token(self):
+        a = AuthenticationToken(access_token=FAKE_DATA["access_token"])
+        self.assertEqual(a.access_token, FAKE_DATA["access_token"])
+
+    def test_init_client_token(self):
+        a = AuthenticationToken(client_token=FAKE_DATA["client_token"])
+        self.assertEqual(a.client_token, FAKE_DATA["client_token"])
 
     def test_init_positional(self):
         a = AuthenticationToken(FAKE_DATA["username"],
@@ -73,59 +155,6 @@ class Yggdrasil(unittest.TestCase):
         self.assertEqual(a.username, FAKE_DATA["username"])
         self.assertEqual(a.access_token, FAKE_DATA["access_token"])
         self.assertEqual(a.client_token, FAKE_DATA["client_token"])
-
-
-class MakeRequest(unittest.TestCase):
-    def test_make_request_http_method(self):
-        req = _make_request(AUTHSERVER, "authenticate", {"Billy": "Bob"})
-        self.assertEqual(req.request.method, "POST")
-
-    def test_make_request_json_dump(self):
-        data = {"Marie": "McGee",
-                "George": 1,
-                "Nestly": {
-                    "Nestling": "Nestling's tail"
-                    },
-                "Listly": ["listling1", 2, "listling 3"]
-                }
-
-        req = _make_request(AUTHSERVER, "authenticate", data)
-        self.assertEqual(req.request.body, json.dumps(data))
-
-    def test_make_request_url(self):
-        URL = "https://authserver.mojang.com/authenticate"
-        req = _make_request(AUTHSERVER, "authenticate", {"Darling": "Diary"})
-        self.assertEqual(req.request.url, URL)
-
-
-class RaiseFromRequest(unittest.TestCase):
-    def test_raise_from_erroneous_request(self):
-        err_req = requests.Request()
-        err_req.status_code = 401
-        err_req.json = mock.MagicMock(
-            return_value={"error": "ThisIsAnException",
-                          "errorMessage": "Went wrong."})
-
-        with self.assertRaises(YggdrasilError) as e:
-            _raise_from_request(err_req)
-            self.assertEqual(e, "[401]) ThisIsAnException: Went Wrong.")
-
-    def test_raise_from_erroneous_request_without_error(self):
-        err_req = requests.Request()
-        err_req.status_code = 401
-        err_req.json = mock.MagicMock(return_value={"goldfish": "are pretty."})
-
-        with self.assertRaises(YggdrasilError) as e:
-            _raise_from_request(err_req)
-
-            self.assertEqual(e, "Malformed error message.")
-
-    def test_raise_from_healthy_request(self):
-        req = requests.Request()
-        req.status_code = 200
-        req.json = mock.MagicMock(return_value={"vegetables": "are healthy."})
-
-        self.assertIs(_raise_from_request(req), None)
 
 
 class AuthenticatedAuthenticationToken(unittest.TestCase):
@@ -187,3 +216,56 @@ class InvalidateAuthenticationToken(unittest.TestCase):
 class JoinAuthenticationToken(unittest.TestCase):
     # TODO: Make me!
     pass
+
+
+class MakeRequest(unittest.TestCase):
+    def test_make_request_http_method(self):
+        req = _make_request(AUTHSERVER, "authenticate", {"Billy": "Bob"})
+        self.assertEqual(req.request.method, "POST")
+
+    def test_make_request_json_dump(self):
+        data = {"Marie": "McGee",
+                "George": 1,
+                "Nestly": {
+                    "Nestling": "Nestling's tail"
+                    },
+                "Listly": ["listling1", 2, "listling 3"]
+                }
+
+        req = _make_request(AUTHSERVER, "authenticate", data)
+        self.assertEqual(req.request.body, json.dumps(data))
+
+    def test_make_request_url(self):
+        URL = "https://authserver.mojang.com/authenticate"
+        req = _make_request(AUTHSERVER, "authenticate", {"Darling": "Diary"})
+        self.assertEqual(req.request.url, URL)
+
+
+class RaiseFromRequest(unittest.TestCase):
+    def test_raise_from_erroneous_request(self):
+        err_req = requests.Request()
+        err_req.status_code = 401
+        err_req.json = mock.MagicMock(
+            return_value={"error": "ThisIsAnException",
+                          "errorMessage": "Went wrong."})
+
+        with self.assertRaises(YggdrasilError) as e:
+            _raise_from_request(err_req)
+            self.assertEqual(e, "[401]) ThisIsAnException: Went Wrong.")
+
+    def test_raise_from_erroneous_request_without_error(self):
+        err_req = requests.Request()
+        err_req.status_code = 401
+        err_req.json = mock.MagicMock(return_value={"goldfish": "are pretty."})
+
+        with self.assertRaises(YggdrasilError) as e:
+            _raise_from_request(err_req)
+
+            self.assertEqual(e, "Malformed error message.")
+
+    def test_raise_from_healthy_request(self):
+        req = requests.Request()
+        req.status_code = 200
+        req.json = mock.MagicMock(return_value={"vegetables": "are healthy."})
+
+        self.assertIs(_raise_from_request(req), None)
