a589c1e61f4d634d808fdd6d77cdc06cf4cbfeb1
Jeppe Klitgaard <jeppe@dapj.dk>
2015-04-11 17:47:25 +0200

    Added exceptions for serialization and deserialization. These will come in handy later

--- minecraft/exceptions.py
+++ minecraft/exceptions.py
@@ -5,5 +5,19 @@ Contains the `Exceptions` used by this library.
 
 class YggdrasilError(Exception):
     """
-    Base `Exception` for the Yggdrasil authentication service.
+    Base ``Exception`` for the Yggdrasil authentication service.
+    """
+
+
+class DeserializationError(Exception):
+    """
+    ``Exception`` raised when something went wrong during the deserialization
+    process.
+    """
+
+
+class SerializationError(Exception):
+    """
+    ``Exception`` raised when something went wrong during the serialization
+    process.
     """
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,4 +1,5 @@
 from minecraft.exceptions import YggdrasilError
+from minecraft.exceptions import DeserializationError, SerializationError
 
 import unittest
 
@@ -11,5 +12,29 @@ class RaiseYggdrasilError(unittest.TestCase):
     def test_raise_yggdrasil_error_message(self):
         with self.assertRaises(YggdrasilError) as e:
             raise YggdrasilError("Error!")
+
+        self.assertEqual(str(e.exception), "Error!")
+
+
+class RaiseDeserializationError(unittest.TestCase):
+    def test_raise_deserialization_error(self):
+        with self.assertRaises(DeserializationError):
+            raise DeserializationError
+
+    def test_raise_deserialization_error_message(self):
+        with self.assertRaises(DeserializationError) as e:
+            raise DeserializationError("Error!")
+
+        self.assertEqual(str(e.exception), "Error!")
+
+
+class RaiseSerializationError(unittest.TestCase):
+    def test_raise_serialization_error(self):
+        with self.assertRaises(SerializationError):
+            raise SerializationError
+
+    def test_raise_serialization_error_message(self):
+        with self.assertRaises(SerializationError) as e:
+            raise SerializationError("Error!")
 
         self.assertEqual(str(e.exception), "Error!")
