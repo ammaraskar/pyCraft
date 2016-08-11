d0169b2d0b2d60fcbdfb6f999d80d7b264097986
joo <john.macinroe@gmail.com>
2016-08-11 10:40:50 +0100

    Add support for Minecraft pre-release 16w32a (protocol 301).

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,10 +8,6 @@ class VersionTest(unittest.TestCase):
     def test_module_version_is_a_valid_pep_386_strict_version(self):
         SV(minecraft.__version__)
 
-    def test_minecraft_version_is_a_valid_pep_386_strict_version(self):
-        for version in minecraft.SUPPORTED_MINECRAFT_VERSIONS.keys():
-            SV(version)
-
     def test_protocol_version_is_an_int(self):
         for version in minecraft.SUPPORTED_PROTOCOL_VERSIONS:
             self.assertTrue(type(version) is int)
