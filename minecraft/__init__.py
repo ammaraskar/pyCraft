3115778bde326002661873ce96da7ddb4d881144
Nigel Todman <veritas@vts-tech.org>
2017-05-05 02:43:19 -0400

    Added 17w18a
    Added 17w18a

--- minecraft/__init__.py
+++ minecraft/__init__.py
@@ -52,6 +52,7 @@ SUPPORTED_MINECRAFT_VERSIONS = {
     '17w17a':    324,
     '17w17b':    325,
     '17w18a':    326,
+    '17w18b':    327,
 }
 
 SUPPORTED_PROTOCOL_VERSIONS = sorted(SUPPORTED_MINECRAFT_VERSIONS.values())
