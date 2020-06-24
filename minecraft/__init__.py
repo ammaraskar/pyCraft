0d28271c962f6f4958cc685dd45bfa86daafc076
Sillyfrog <sillyfrog@users.noreply.github.com>
2020-06-24 13:50:29 +1000

    v1.16 release support

--- minecraft/__init__.py
+++ minecraft/__init__.py
@@ -247,6 +247,7 @@ SUPPORTED_MINECRAFT_VERSIONS = {
     '1.16 Pre-release 4':   727,
     '1.16 Pre-release 5':   729,
     '1.16 Release Candidate 1':734,
+    '1.16':                 735,
 }
 
 # Those Minecraft versions supported by pyCraft which are "release" versions,
